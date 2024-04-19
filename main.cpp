7d3ddfcaad8b935209975801cf874d6ebd6541eb
wahibonae <wahibabkari@gmail.com>
2024-04-20 00:34:37 +0100

    updated

--- main.cpp
+++ main.cpp
@@ -1,5 +1,8 @@
 #include <iostream>
+#include <string>
 using namespace std;
+
+// Q1
 
 typedef struct date {
     int jour;
@@ -7,6 +10,17 @@ typedef struct date {
     int annee;
 }date;
 
+class Depot {
+    private:
+        int id;
+        string adresse;
+        int capacite;
+        // Dépôt – Stock (composition 1 à *)
+        
+    public:
+
+};
+
 int main(){
-    
+
 }
